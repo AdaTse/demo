77fa404d68e8056ad92b874325f10d3f3278f3b6
AdaTse <AdaTse19880301@gmail.com>
2016-04-23 17:42:17 +0800

    RESTful practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
   resources :people
+  resources :events
   get "welcome/say_hello" => "welcome#say"
   get "welcome" => "welcome#index"
   get "events" => "events#index"
   root :to => "welcome#index"
 
-  match ' :controller(/:action(/:id(.:format)))' , :via => :all
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 end
