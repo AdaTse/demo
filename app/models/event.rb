3d4271369ccb6da8c16c604a977b78393e72a5e9
AdaTse <AdaTse19880301@gmail.com>
2016-04-26 21:07:19 +0800

    ActiveRecord 基本操作與關聯設計

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
+  has_one :location
+  belongs_to :category
+  has_many :attendees
   validates_presence_of :name
 end
